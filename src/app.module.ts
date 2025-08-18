c39c734959dab71ee4f2d360e336c8f0a3aaeb89
Faruuuqqq <faruqmahdison@gmail.com>
2025-08-18 09:52:34 +0700

    feat(queue): implement Queue module with controller and service, add setup redis for caching support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { redisStore } from 'cache-manager-redis-store';
+import { CacheModule } from '@nestjs/cache-manager';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { QueueModule } from './queue/queue.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        store: await redisStore({
+          host: configService.get('REDIS_HOST'),
+          port: configService.get('REDIS_PORT'),
+          ttl: configService.get('REDIS_TTL'),
+        }),
+      }),
+    }),
     PrismaModule,
+    QueueModule,
   ],
   controllers: [AppController],
   providers: [AppService],
